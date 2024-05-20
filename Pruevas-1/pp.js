7cb86f306b0a84b70df0016226e3970f3827df47
Clwser32 <165855703+Clwser32@users.noreply.github.com>
2024-05-19 23:27:47 -0300

    Add files via upload

---
+++ Pruevas-1/pp.js
@@ -0,0 +1,11 @@
+const btnAbrir = document.querySelector("#btnabrircate");
+const btnCerrar = document.querySelector("#btncerrarcate");
+const modal = document.querySelector("#modal");
+
+btnAbrir.addEventListener("click", () => {
+  modal.showmodal;
+});
+
+btnCerrar.addEventListener("click", () => {
+  modal.closest();
+});
